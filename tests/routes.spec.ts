e055ea50901c585641bd83bece3bddea642264af
Diogo <diogojorge1401@gmail.com>
2023-06-30 18:33:36 -0300

    🧪 test: add test to route /math

--- tests/routes.spec.ts
+++ tests/routes.spec.ts
@@ -11,9 +11,16 @@ describe('GET /', () => {
 });
 
 describe('GET /math', () => {
-  it('should return 200 and get `Sum(10, 5): 15, Sub(10, 5): 5`', async () => {
+  it("should return 200 and get `Sum(0, 0): 0, Sub(0, 0): 0` when doesn't pass parameters", async () => {
     const request = await superoak(app);
 
-    await request.get('/math').expect(200, 'Sum(10, 5): 15, Sub(10, 5): 5');
+    await request.get('/math').expect(200, 'Sum(0, 0): 0, Sub(0, 0): 0');
+  });
+  it('should return 200 and get `Sum(10, 5): 15, Sub(10, 5): 5` when do pass 10 and 5 as parameters', async () => {
+    const request = await superoak(app);
+
+    await request
+      .get('/math?a=10&b=5')
+      .expect(200, 'Sum(10, 5): 15, Sub(10, 5): 5');
   });
 });
