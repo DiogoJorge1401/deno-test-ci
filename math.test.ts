42759415a33e8733f7e5907717e24bac69b8cb24
Diogo <diogojorge1401@gmail.com>
2023-06-30 10:58:33 -0300

    ♻️ refactor: move functions to a class Math

--- math.test.ts
+++ math.test.ts
@@ -1,17 +1,19 @@
 import { assertEquals } from 'https://deno.land/std@0.192.0/testing/asserts.ts';
 import { describe, it } from 'https://deno.land/std@0.192.0/testing/bdd.ts';
-import { sub, sum } from './math.ts';
+import { Math } from './math.ts';
 
-describe('math.ts', () => {
-  it('should return 3 when passed 1 and 2', () => {
-    const actual = sum(1, 2);
+describe('Math', () => {
+  const math = new Math(1, 2);
+
+  it('sum should return 3 when passed 1 and 2', () => {
+    const actual = math.sum();
     const expected = 3;
     assertEquals(actual, expected);
   });
 
-  it('should return -2 when passed -1 and 1', () => {
-    const actual = sub(-1, 1);
-    const expected = -2;
+  it('sub should return -1 when passed 1 and 2', () => {
+    const actual = math.sub();
+    const expected = -1;
     assertEquals(actual, expected);
   });
 });
