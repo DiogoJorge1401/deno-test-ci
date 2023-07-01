0e97d4bc78edd9733b8595c30ea921bf31cc5f0b
Diogo <diogojorge1401@gmail.com>
2023-07-01 15:35:51 -0300

    ♻️ refactor: update errorHandler

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,21 +1,12 @@
-import {
-  Context,
-  Next,
-  isHttpError
-} from 'https://deno.land/x/oak@v12.5.0/mod.ts';
+import { Context, Next } from 'https://deno.land/x/oak@v12.5.0/mod.ts';
 
 export const errorHandler = async (context: Context, next: Next) => {
   try {
     await next();
   } catch (err) {
-    if (isHttpError(err)) {
-      context.response.status = err.status;
-      const { message, status, stack } = err;
-      context.response.body = { message, status, stack };
-      context.response.type = 'json';
-    } else {
-      console.log(err);
-      throw err;
-    }
+    context.response.status = err.status;
+    const { message, status, stack } = err;
+    context.response.body = { message, status, stack };
+    context.response.type = 'json';
   }
 };
