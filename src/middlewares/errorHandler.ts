d13c8e210db290d9cb38237f81617890c534cf9d
Diogo <diogojorge1401@gmail.com>
2023-07-01 14:28:20 -0300

    ♻️ refactor: update error response

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -11,13 +11,8 @@ export const errorHandler = async (context: Context, next: Next) => {
     if (isHttpError(err)) {
       context.response.status = err.status;
       const { message, status, stack } = err;
-      if (context.request.accepts('json')) {
-        context.response.body = { message, status, stack };
-        context.response.type = 'json';
-      } else {
-        context.response.body = `${status} ${message}\n\n${stack ?? ''}`;
-        context.response.type = 'text/plain';
-      }
+      context.response.body = { message, status, stack };
+      context.response.type = 'json';
     } else {
       console.log(err);
       throw err;
